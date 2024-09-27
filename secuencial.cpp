c9ee0a8dabe17a8588cc1342646216a76ee4ca35
LVelezm <lvelezm@unjbg.edu.pe>
2024-09-27 15:09:46 -0500

    Añade un menu de opciones y nueva función secuencialOrd por si los elementos estan ordenados en el arreglo

--- secuencial.cpp
+++ secuencial.cpp
@@ -13,20 +13,51 @@ int secuencialDes(int n, int A[], int num){
 		return pos;
 	} 
 }
+int secuencialOrd(int n, int A[], int num){
+	int i=0;
+	int pos;
+	while(i<=n&&A[i]<num){
+		i=i+1;
+	}
+	if(i>n||A[i]!=num){
+		pos=-i;
+		return pos;
+	}else{
+		pos=i;
+		return pos;
+	} 
+}
 int main(){
 	int n;
 	int num;
 	int pos;
+	int opcion;
+	cout<<"Este programa buscara un dato de manera secuencial ordenada o desordenada"<<endl;
 	cout<<"Cantidad de elementos del arreglo: ";
 	cin>>n;
 	int A[n];
 	for(int i=0; i<n;i++){
-		cout<<"numero "<< i+1 <<": ";
+		cout<<"numero "<< i <<": ";
 		cin>>A[i];  
 	}
 	cout<<"Indique el numero a buscar: ";
 	cin>>num;
-	pos=secuencialDes(n,A,num);
-	cout<<"EL numero esta en la posicion: "<<pos+1;
+	cout<<"Elija una opcion:"<<endl;
+	cout<<"1. Busqueda secuencial desordenada"<<endl;
+	cout<<"2. Busqueda secuencial ordenada"<<endl;
+	cout<<"opcion: ";
+	cin>>opcion;
+	switch(opcion){
+		case 1:
+			pos=secuencialDes(n,A,num);
+			cout<<"EL numero esta en la posicion: "<<pos;
+			break;
+		case 2:
+			pos=secuencialOrd(n,A,num);
+			cout<<"EL numero esta en la posicion: "<<pos;
+			break;
+		default:
+			cout<<"Opcion invalida"<<endl;
+	}
 	return 0;
 }
